08e8bda4c4e478df620553778a06af6ee1d39094
Damco Space Inc <damcospace@Damcos-MacBook-Pro.local>
2026-08-05 20:52:21 -0400

    Add official Nacha ACH/eCheck logo to accepted payment badges

--- .open-next/cloudflare/init.js
+++ .open-next/cloudflare/init.js
@@ -47,7 +47,7 @@ function initRuntime() {
   };
   Object.assign(globalThis, {
     Request: CustomRequest,
-    __BUILD_TIMESTAMP_MS__: 1785977013864,
+    __BUILD_TIMESTAMP_MS__: 1785977449242,
     __NEXT_BASE_PATH__: "",
     __ASSETS_RUN_WORKER_FIRST__: false,
     __TRAILING_SLASH__: false,
